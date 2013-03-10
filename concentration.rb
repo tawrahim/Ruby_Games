8d6838bf7430df7e98249f27e36429926c150cca
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-10 16:40:12 -0400

    populate method

--- concentration.rb
+++ concentration.rb
@@ -1,1 +1,30 @@
 #Memory concentration Game
+# Author: Tawheed Abdul-Raheem
+# url: www.tawheedraheem.com
+
+# This method method populates the concentration game board
+# populating and returning a 5*4 two-dimensional array
+# Initializes all the values to 0, then randomly populate
+# then randomly populate numbers from 1-10 in different cells
+# making sure that each number is used twice
+def populate
+	# populate the array, with 0
+	board = []
+	for i in (1..4);
+		board << [0,0,0,0]
+	end
+	#print board[3].size
+
+	# loop through each of the rows in the array
+	for x in (0..board.size-1);
+		for j in (0..board[x].size-1);
+			board[x][j] = rand(10)
+			#check to see if the number has been used before
+
+		end
+	end
+	
+	print board
+end
+
+populate
