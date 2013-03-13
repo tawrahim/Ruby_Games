04ef6d672c66a7fdc5414608e67150163de7228f
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-12 22:11:08 -0400

    Created stub for the bank Teller program

---
+++ bankteller.rb
@@ -0,0 +1,43 @@
+# This is my solution to the bank teller project
+# on www.learnstreet.com
+# Author: Tawheed Abdul-Raheem
+# url: www.tawheedraheem.com
+
+class BankTeller
+
+	# create a new account and return its name. Password
+	# is the supplied PIN of the account returned
+	# Also establish an account balance
+	# Basically return a hash of the information that was sent
+	def add_account(name,password,accounts)
+		balance = 0.0
+		new_account = {"name" => name,"password" => password, "accounts" => accounts,"balance" => balance}
+		return new_account
+	end
+
+	# This method gets the balance of the account
+	def get_balance(accounts)
+		accounts = {"balance"=>0}
+		accounts["balance"]
+		return accounts["balance"]
+	end
+
+	# This method adds amount to the supplied account
+	def do_deposit(accounts,amount)
+
+	end
+
+	# This method withdraws money from the account
+	def do_withdraw(accounts,amount)
+	end
+
+	# Transfers money from one bank account to the other
+	# from the debtor to creditor. check for insufficient funds
+	def do_transfer(debtor,creditor,amount)
+		
+	end
+end
+
+accnt = BankTeller.new
+puts accnt.add_account("Tawheed", "kayode90", "discover")
+puts accnt.get_balance("discover")
